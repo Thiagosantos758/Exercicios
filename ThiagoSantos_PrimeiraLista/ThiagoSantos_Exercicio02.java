bd54594e3bc0f79866f5df47b0af71bb7916674e
pelédohtml <131814290+Thiagosantos758@users.noreply.github.com>
2023-05-15 16:48:53 -0300

    Update ThiagoSantos_Exercicio02.java
    Comentários 👍

--- ThiagoSantos_PrimeiraLista/ThiagoSantos_Exercicio02.java
+++ ThiagoSantos_PrimeiraLista/ThiagoSantos_Exercicio02.java
@@ -1,11 +1,15 @@
-
+/*
+* Descrição: Recebe quatro números, os exponencia e soma
+* Autor: Thiago
+* Data: 15/05/2023
+*/
 import java.util.Scanner;
 public class Ex2 {
     public static void main(String [] args){
         Scanner input = new Scanner (System.in);
         double n1;
-        System.out.println("Digite o primeiro número:");
-        n1 = input.nextDouble();
+        System.out.println("Digite o primeiro número:");//Mostra a mensagem na String na tela
+        n1 = input.nextDouble(); //faz com que a variável n1 receba um valor digitado pelo usuário
         System.out.println("Digite o segundo número:");
         double n2 = input.nextDouble();
         System.out.println("Digite o terceiro número:");
