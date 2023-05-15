350370cce27525d8723aaf3d9809f96549373c95
pelédohtml <131814290+Thiagosantos758@users.noreply.github.com>
2023-05-15 17:09:40 -0300

    Update ThiagoSantos_Exercicio06.java
    Comentários 👍

--- ThiagoSantos_PrimeiraLista/ThiagoSantos_Exercicio06.java
+++ ThiagoSantos_PrimeiraLista/ThiagoSantos_Exercicio06.java
@@ -6,11 +6,11 @@
 import java.util.Scanner;
 public class ThiagoSantos_Exercicio06 {
     public static void main(String [] args){
-        Scanner input = new Scanner (System.in);
+        Scanner input = new Scanner (System.in); //Faz com que a vaiável input receba informações digitadas pelo usuário
         double pi = 3.1415; //Atribui um valor constante de 3,1415 para a variável pi
         System.out.println("Digite o valor do raio do círculo: ");
         double raio = input.nextDouble();
-        double per = raio*(pi*2);
+        double per = raio*(pi*2); //calcula o perímetro e salva o resultado na variável "per"
         System.out.print("O perímetro do circulo é: " + per);
         input.close();
     }
