d935c09cebfba0831442fa2ee4290afa3fc5ceef
pelédohtml <131814290+Thiagosantos758@users.noreply.github.com>
2023-05-15 16:31:34 -0300

    Update and rename Ex1.java to ThiagoSantos_Exercicio01.java

--- ThiagoSantos_PrimeiraLista/ThiagoSantos_Exercicio01.java
+++ ThiagoSantos_PrimeiraLista/ThiagoSantos_Exercicio01.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Ex1 {
+public class ThiagoSantos_Exercicio01 {
     public static void main(String [] args){
         Scanner input = new Scanner (System.in);
         System.out.println("Digite um número");
