443fdee8054dddd187db243a2546c3fcf0c1e587
pelédohtml <131814290+Thiagosantos758@users.noreply.github.com>
2023-05-15 17:07:28 -0300

    Update ThiagoSantos_Exercicio06.java
    Comentários 👍

--- ThiagoSantos_PrimeiraLista/ThiagoSantos_Exercicio06.java
+++ ThiagoSantos_PrimeiraLista/ThiagoSantos_Exercicio06.java
@@ -1,8 +1,13 @@
+/*
+* Descrição: Calcula o perímetro do círculo a partir do valor do raio inserido pelo usuário
+* Autor: Thiago
+* Data: 15/05/2023
+*/
 import java.util.Scanner;
 public class ThiagoSantos_Exercicio06 {
     public static void main(String [] args){
         Scanner input = new Scanner (System.in);
-        double pi = 3.1415;
+        double pi = 3.1415; //Atribui um valor constante de 3,1415 para a variável pi
         System.out.println("Digite o valor do raio do círculo: ");
         double raio = input.nextDouble();
         double per = raio*(pi*2);
